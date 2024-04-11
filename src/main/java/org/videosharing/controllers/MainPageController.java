5af5f08e837bec72d5fd7ece4d8d4c5fcd018643
iammachine <iammachineproduction@gmail.com>
2024-04-11 12:20:45 +0300

    back to old frontend view

--- src/main/java/org/videosharing/controllers/MainPageController.java
+++ src/main/java/org/videosharing/controllers/MainPageController.java
@@ -1,14 +1,12 @@
 package org.videosharing.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.videosharing.services.VideoService;
 
 import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -20,11 +18,11 @@ public class MainPageController {
     public ModelAndView index() {
         HashMap<String, Object> model = new HashMap<>();
         model.put("videos", videoService.getAllVideoNames());
-        return new ModelAndView("index_old", model);
+        return new ModelAndView("index", model);
     }
 
     @GetMapping(value = "/upload")
     public ModelAndView upload() {
-        return new ModelAndView("index_old");
+        return new ModelAndView("index");
     }
 }
